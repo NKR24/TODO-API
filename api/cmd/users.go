403b533ce028655ece31f35736e260311032b72c
Nikita Khvorostin <khvorostinn@gmail.com>
2024-05-20 17:51:55 +0300

    created a new func createNewTodo

--- api/cmd/users.go
+++ api/cmd/users.go
@@ -1,1 +1,27 @@
 package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/google/uuid"
+	"github.com/labstack/echo/v4"
+)
+
+type Todos struct {
+	ID        uuid.UUID `json:"id"`
+	Note      string    `json:"note"`
+	Completed bool      `json:"completed"`
+}
+
+func createNewNote(c echo.Context) error {
+	newTodos := new(Todos)
+	newTodos.ID = uuid.New()
+	c.Bind(newTodos)
+	key := fmt.Sprintf("Todos:%s", newTodos.ID)
+	_, err := rh.JSONSet(key, ".", newTodos)
+	if err != nil {
+		return c.NoContent(http.StatusBadRequest)
+	}
+	return c.JSON(http.StatusCreated, newTodos)
+}
