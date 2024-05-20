5e1d349b6b9795f4c41cb8eb02851280ae8d5b36
Nikita Khvorostin <khvorostinn@gmail.com>
2024-05-20 17:39:25 +0300

    filled the globals.go file with variables to create a connection to the database

--- api/cmd/globals.go
+++ api/cmd/globals.go
@@ -1,1 +1,24 @@
 package main
+
+import (
+	"context"
+
+	"github.com/nitishm/go-rejson/v4"
+	"github.com/redis/go-redis/v9"
+)
+
+var (
+	ctx context.Context
+	rbd *redis.Client
+	rh  *rejson.Handler
+)
+
+func init() {
+	ctx := context.Background()
+	rbd := redis.NewClient(&redis.Options{
+		DB:   0,
+		Addr: "localhost:6379",
+	})
+	rh := rejson.NewReJSONHandler()
+	rh.SetGoRedisClientWithContext(ctx, rbd)
+}
